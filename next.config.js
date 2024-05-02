41a4f9444b97ddf0fa912803f1fcdce3c6c84463
Ron Rounsifer <ronrounsifer@gmail.com>
2024-05-02 11:59:54 -0500

    Update next.config.js to not optimize images.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
     output: "export",
+    images: {unoptimized: true},
     reactStrictMode: true,
     swcMinify: true,
     webpack: function (config) {
