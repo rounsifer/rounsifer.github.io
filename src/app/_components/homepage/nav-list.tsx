8238e935ee67d1b4143c2d9571fb5e9cd3c009e7
Ron Rounsifer <ronrounsifer@gmail.com>
2024-05-02 15:44:32 -0500

    Update tagline formatting

--- src/app/_components/homepage/nav-list.tsx
+++ src/app/_components/homepage/nav-list.tsx
@@ -11,7 +11,7 @@ export const NavList = () => {
                 </li>
 
                 <li className="text-white/85 font-light tracking-wide w-2/3 sm:w-full sm:text-start text-center italic">
-                    just another software engineer that likes to build things
+                    just another <span className="font-semibold text-white">software engineer</span> that likes to build things
                 </li>
                 <ul className="w-full flex flex-row gap-1 justify-center sm:justify-start">
                     <li className="text-white/50">
@@ -20,7 +20,6 @@ export const NavList = () => {
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
                             </svg>
-
                         </a>
                     </li>
                     <li className="text-white/50">
